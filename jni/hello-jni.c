c4b6bb193dd14581546f6c28be51b8958328ed3c
Jennifer Guo <guojy.bj@gmail.com>
2012-03-21 15:42:42 +0800

    first commit

---
+++ jni/hello-jni.c
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include <string.h>
+#include <jni.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DEBUG
+
+/* struct in6_addr */
+#include <netinet/in.h>
+/* getifaddrs, freeifaddrs */
+#include <sys/types.h>
+
+/* inet_ntop, inet_pton */
+#include <arpa/inet.h>
+
+#if defined(DEBUG)
+#define DG(a, b...) printf("[%s][%s][%d]"a, __FILE__, __func__, __LINE__, ##b)
+#else
+#define DG(a, b...)
+#endif
+
+#include <android/log.h>
+
+#define ER(a, b...) printf("[%s][%s][%d]"a, __FILE__, __func__, __LINE__, ##b)
+
+
+/* This is a trivial JNI example where we use a native method
+ * to return a new VM String. See the corresponding Java source
+ * file located at:
+ *
+ *   apps/samples/hello-jni/project/src/com/example/HelloJni/HelloJni.java
+ */
+jstring
+Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
+                                                  jobject thiz )
+{
+    struct in6_addr addr6;
+    char *ifname = "eth0";
+    char address[64];
+    
+	__android_log_write(ANDROID_LOG_DEBUG, "mee", "just here");
+    get_linklocal_ipv6addr(&addr6, ifname);
+    return (*env)->NewStringUTF(env, "Hello World. change change!!");
+}
+
+
+int get_linklocal_ipv6addr(struct in6_addr *addr6, char *iface)
+{
+#define IF_INET6 "/proc/net/if_inet6"
+	char buf[256];
+    char str[128], address[64];
+    char *addr, *index, *prefix, *scope, *flags, *name;
+    char *delim = " \t\n", *p, *q;
+    FILE *fp;
+    int count;
+    
+    if (!addr6 || !iface) {
+        ER("addr6 and iface can't be NULL!\n");
+        return -1;
+    }
+    
+    if (NULL == (fp = fopen(IF_INET6, "r"))) {
+        ER("");
+		__android_log_write(ANDROID_LOG_ERROR, "mee", "can't read. fopen failed");
+        perror("fopen error");
+        return -1;
+    }
+    
+#define IPV6_ADDR_LINKLOCAL 0x0000U
+    while (fgets(str, sizeof(str), fp)) {
+        DG("str:%s", str);
+        addr = strtok(str, delim);
+        index = strtok(NULL, delim);
+        prefix = strtok(NULL, delim);
+        scope = strtok(NULL, delim);
+        flags = strtok(NULL, delim);
+        name = strtok(NULL, delim);
+		sprintf(buf, "addr:%s, index:0x%s, prefix:0x%s, scope:0x%s, flags:0x%s, name:%s\n",
+           addr, index, prefix, scope, flags, name);
+		__android_log_write(ANDROID_LOG_DEBUG, "mee", buf);
+        DG("addr:%s, index:0x%s, prefix:0x%s, scope:0x%s, flags:0x%s, name:%s\n",
+           addr, index, prefix, scope, flags, name);
+        
+//        if (strcmp(name, iface))
+//            continue;
+        
+        /* Just get IPv6 linklocal address */
+        if (IPV6_ADDR_LINKLOCAL != (unsigned int)strtoul(scope, NULL, 16))
+            continue;
+        
+        memset(address, 0x00, sizeof(address));
+        p = addr;
+        q = address;
+        count = 0;
+        while (*p != ' ') {
+            if (count == 4) {
+                *q++ = ':';
+                count = 0;
+            }
+            *q++ = *p++;
+            count++;
+        }
+		sprintf(buf,"find out %s's linklocal IPv6 address: %s\n", iface, address);
+		__android_log_write(ANDROID_LOG_DEBUG, "mee", buf);
+        DG("find out %s's linklocal IPv6 address: %s\n", iface, address);
+        
+        inet_pton(AF_INET6, address, addr6);
+        break;
+    }
+#undef IPV6_ADDR_LINKLOCAL
+    
+    fclose(fp);
+    return 0;
+}
---
+++ obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,124 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/string.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/android/api-level.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/malloc.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/jni.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/stdio.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/stdint.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/alloca.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/strings.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/memory.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/netinet/in.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/endian.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/endian.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/socket.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/socket.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/sockios.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/sockios.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/uio.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/in.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/byteorder.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/byteorder/little_endian.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/byteorder/swab.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/byteorder/generic.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/in6.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/ipv6.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/netinet/in6.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/arpa/inet.h \
+ E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/android/log.h
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/string.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/android/api-level.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/jni.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/stdio.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/strings.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/memory.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/netinet/in.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/endian.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/sys/endian.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/socket.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/socket.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/sockios.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/sockios.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/uio.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/in.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/asm/byteorder.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/byteorder/swab.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/byteorder/generic.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/in6.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/linux/ipv6.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/netinet/in6.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/arpa/inet.h:
+
+E:/android-ndk-r7b/platforms/android-5/arch-arm/usr/include/android/log.h:
